259f8298c8dfa715854040b3811854ccef42cfb8
juliusj03 <116172471+juliusj03@users.noreply.github.com>
2025-05-06 16:08:25 +0200

    fd

---
+++ Week 1-2 - general intro to exploratory modelling/tmp1iwKC/tmp18148/model/__init__.py
@@ -0,0 +1,5 @@
+# This file marks the directory as a Python package.
+# You can use this file to initialize package-level variables or import submodules.
+
+# Example: Importing a submodule
+# from .module_name import some_function
